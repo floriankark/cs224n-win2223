a5ce07bcc78af472c8d7955b6c1afe0efcd339cf
floriankark <florian.kark@gmail.com>
2023-04-23 19:25:41 +0200

    complete sgd

--- a2/sgd.py
+++ a2/sgd.py
@@ -86,7 +86,8 @@ def sgd(f, x0, step, iterations, postprocessing=None, useSaved=False,
 
         loss = None
         ### YOUR CODE HERE (~2 lines)
-
+        loss, grad = f(x)
+        x -= step * grad
         ### END YOUR CODE
 
         x = postprocessing(x)
