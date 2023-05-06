aa7f36caa9d75499d4c6d2262e2af1aa0fd19648
floriankark <florian.kark@gmail.com>
2023-05-06 14:35:48 +0200

    implement pad_sents

--- a4/utils.py
+++ a4/utils.py
@@ -35,8 +35,9 @@ def pad_sents(sents, pad_token):
     sents_padded = []
 
     ### YOUR CODE HERE (~6 Lines)
-
-
+    max_len = max([len(sent) for sent in sents])
+    for sent in sents:
+        sents_padded.append(sent + [pad_token] * (max_len - len(sent)))
     ### END YOUR CODE
 
     return sents_padded
